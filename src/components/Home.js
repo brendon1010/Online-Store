ed69b2e7e7427429afb684acc9f404c0da7a6d5f
Brendon990 <stvn9909@gmail.com>
2023-12-28 18:33:45 +0200

    maped products

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,28 @@
-import { Carousel } from "react-bootstrap";
+import { Carousel, Card } from "react-bootstrap";
+import Store from "./Store";
+import About from "./About";
 
 export default function Home() {
   return (
-    <span className="important">
-    <Carousel data-bs-theme="dark">
-      <Carousel.Item>
+    <div>
+      <br/>
+    <Carousel>
+      <Carousel.Item style={{backgroundColor: 'black'}}>
+        <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="back" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item style={{backgroundColor: 'black'}}>
+      <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="back" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item style={{backgroundColor: 'black'}}>
+      <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="back" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
@@ -25,6 +31,23 @@ export default function Home() {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-    </span>
+    <br/>
+    <br/>
+    <Card>
+      <Card.Img className="banner" src="https://t4.ftcdn.net/jpg/05/49/86/39/360_F_549863991_6yPKI08MG7JiZX83tMHlhDtd6XLFAMce.jpg" alt="Card image" />
+      <Card.ImgOverlay style={{color: 'white'}}>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </Card.Text>
+        <Card.Text>Last updated 3 mins ago</Card.Text>
+      </Card.ImgOverlay>
+    </Card>
+    <br/>
+    <br/>
+    <Store/><br/><hr/>
+    <About/>
+    </div>
   );
 }
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,5 +1,35 @@
+import { Button, Card, Container, Row, Col } from "react-bootstrap"
+
 export default function Store(){
+
+    const items = [
+        {name: 'item1', image: 'https://www.aaronfaber.com/wp-content/uploads/2017/03/product-placeholder-wp.jpg', price: 10, description: 'product description'},
+        {name: 'item2', image: 'https://www.aaronfaber.com/wp-content/uploads/2017/03/product-placeholder-wp.jpg', price: 320, description: 'product description'},
+        {name: 'item3', image: 'https://www.aaronfaber.com/wp-content/uploads/2017/03/product-placeholder-wp.jpg', price: 140, description: 'product description'},
+        {name: 'item4', image: 'https://www.aaronfaber.com/wp-content/uploads/2017/03/product-placeholder-wp.jpg', price: 150, description: 'product description'},
+        {name: 'item5', image: 'https://www.aaronfaber.com/wp-content/uploads/2017/03/product-placeholder-wp.jpg', price: 100, description: 'product description'},
+        {name: 'item6', image: 'https://www.aaronfaber.com/wp-content/uploads/2017/03/product-placeholder-wp.jpg', price: 1550, description: 'product description'},
+    ]
+
     return(
-        <h1>Store</h1>
+        <Container>
+            <Row>
+            {items.map((item) => (
+                <Col>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img style={{width: '286px'}} variant="top" src={item.image}/>
+                    <Card.Body>
+                        <Card.Title>{item.name}</Card.Title>
+                        <Card.Text>
+                            {item.description}<hr/>
+                            R{item.price}
+                        </Card.Text>
+                        <Button variant="primary">Add to cart</Button>
+                    </Card.Body>
+                </Card><br/>
+                </Col>
+            ))}
+            </Row>
+        </Container>
     )
 }
